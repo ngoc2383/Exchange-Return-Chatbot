0ad4418f216638f1f38a92e473a35a5aa3319fcc
Ngoc <ngockhanhmai1102@gmail.com>
2025-02-03 18:45:40 -0500

    Create return_or_exchange function

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ def return_or_exchange():
     choice = None
     while choice is None:
         choice = input("Do you want to return or exchange a product? ")
-        if choice.lower()
+        if choice.lower() != "exchange" or "return":
+            print('Invalid Input. Please choose between "exchange" and "return".')
+            choice = None;
     return choice
